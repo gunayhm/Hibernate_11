cbb7b96cdc4af551233bd4b826ea71f137469221
hasimovagm <gunay.hasimova@kapitalbank.az>
2023-12-05 21:21:05 +0400

    add soft deleted and reformat code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.util.List;
 import java.util.Scanner;
+import org.example.entity.Human;
+import org.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
@@ -17,7 +19,7 @@ public class Main {
                         1: persist
                         2: save
                         3: get
-                        4: find
+                        4: soft deleted
                         5: load
                         6: merge
                         7: update
@@ -37,7 +39,7 @@ public class Main {
                         human.setName(name);
                         human.setAge(age);
                         sessionFactory.persist(human); // persist
-                        sessionFactory.getTransaction().commit();
+                        sessionFactory.getTransaction().commit(); // detached
                     }
                     case 2 -> {
                         sessionFactory.beginTransaction();
@@ -58,7 +60,6 @@ public class Main {
                         int id=scanner.nextInt();
                         Human human=sessionFactory.get(Human.class, id);
                         System.out.println("get object:" + human);
-                        sessionFactory.getTransaction().commit();
                     }
                     case 4->{
                         sessionFactory.beginTransaction();
@@ -76,51 +77,49 @@ public class Main {
                         int id=scanner.nextInt();
                         Human human=sessionFactory.load(Human.class, id);
                         System.out.println("get object:" + human);
-                        sessionFactory.getTransaction().commit();
                     }
                     case 6 -> {
                         sessionFactory.beginTransaction();
-//                        System.out.println("id");
-//                        int id = scanner.nextInt();
-//                        Human human=sessionFactory.get(Human.class,id);
-//                        System.out.println("get object: "+human);
-//                        System.out.println("enter new name: ");
-//                        String name=scanner.next();
-//                        human.setName(name);
-//                        System.out.println("enter  age: ");
-//                        int age=scanner.nextInt();
-//                        human.setAge(age);
-//                        Human merge=sessionFactory.merge(human);  // UPDATE
-//                        System.out.println("Changed: "+merge);
-//                        sessionFactory.getTransaction().commit();
-
-                        Human humanWithoutId=new Human();
-                        humanWithoutId.setId(100);
-                        humanWithoutId.setAge(23);
-                        humanWithoutId.setName("K");
-                        sessionFactory.merge(humanWithoutId); // INSERT
+                        System.out.println("id");
+                        int id = scanner.nextInt();
+                        Human human=sessionFactory.get(Human.class,id);
+                        System.out.println("get object: "+human);
+                        System.out.println("enter new name: ");
+                        String name=scanner.next();
+                        human.setName(name);
+                        System.out.println("enter  age: ");
+                        int age=scanner.nextInt();
+                        human.setAge(age);
+                        Human merge=sessionFactory.merge(human);  // UPDATE
+                        System.out.println("Changed: "+merge);
                         sessionFactory.getTransaction().commit();
+
+//                        Human humanWithoutId=new Human();
+//                        humanWithoutId.setAge(23);
+//                        humanWithoutId.setName("K");
+//                        sessionFactory.merge(humanWithoutId); // INSERT
+//                        sessionFactory.getTransaction().commit();
                     }
                     case 7 -> {
                         sessionFactory.beginTransaction();
-//                        System.out.println("id");
-//                        int id = scanner.nextInt();
-//                        Human human = sessionFactory.get(Human.class, id);
-//                        System.out.println("get object: " + human);
-//                        System.out.println("enter new name: ");
-//                        String name = scanner.next();
-//                        human.setName(name);
-//                        System.out.println("Enter age: ");
-//                        int age = scanner.nextInt();
-//                        human.setAge(age);
-//                        sessionFactory.update(human);
-//                        sessionFactory.getTransaction().commit();
-
-                        Human humanWithoutId=new Human();
-                        humanWithoutId.setAge(23);
-                        humanWithoutId.setName("K");
-                        sessionFactory.update(humanWithoutId);
+                        System.out.println("id");
+                        int id = scanner.nextInt();
+                        Human human = sessionFactory.get(Human.class, id);
+                        System.out.println("get object: " + human);
+                        System.out.println("enter new name: ");
+                        String name = scanner.next();
+                        human.setName(name);
+                        System.out.println("Enter age: ");
+                        int age = scanner.nextInt();
+                        human.setAge(age);
+                        sessionFactory.update(human);
                         sessionFactory.getTransaction().commit();
+
+//                        Human humanWithoutId=new Human();
+//                        humanWithoutId.setAge(23);
+//                        humanWithoutId.setName("K");
+//                        sessionFactory.update(humanWithoutId);
+//                        sessionFactory.getTransaction().commit();
                     }
                     case 9->{
                         sessionFactory.beginTransaction();
